6fd9d21e791f6ab8ebc780e37ef795b638b57635
Isa <jaram.isaac@gmail.com>
2025-10-20 11:01:43 -0500

    Initial Commit

---
+++ messAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  messAI
+//
+//  Created by Isaac Jaramillo on 10/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ messAI/messAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  messAIApp.swift
+//  messAI
+//
+//  Created by Isaac Jaramillo on 10/20/25.
+//
+
+import SwiftUI
+
+@main
+struct messAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
